bea93c0b420c7187f1aabc73f24a7fd884b6908a
Viet Nguyen <viet.nguyen@bmbsoft.com.vn>
2021-05-06 18:15:03 +0700

    Update sidebar

--- packages/RecognitionFE/src/layouts/MainLayout/SideBar/index.js
+++ packages/RecognitionFE/src/layouts/MainLayout/SideBar/index.js
@@ -1,5 +1,6 @@
 import { Avatar, Grid, makeStyles } from '@material-ui/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { COLOR_NAME } from 'src/shared/constants/Colors';
 
 const useStyles = makeStyles(theme => ({
@@ -19,6 +20,7 @@ const useStyles = makeStyles(theme => ({
     '& .menu-title': {
       position: 'relative',
       color: COLOR_NAME.DARK_GREEN,
+      marginBottom: 20,
       '&:before': {
         left: 0,
         width: 240,
@@ -27,6 +29,48 @@ const useStyles = makeStyles(theme => ({
         content: "''",
         position: 'absolute',
         backgroundColor: COLOR_NAME.DARK_GREEN
+      }
+    },
+    '& .nav-menu': {
+      listStyle: 'none',
+      padding: 0,
+      margin: 0,
+      marginTop: 10,
+      '& li': {
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingLeft: 20,
+        '&.active': {
+          backgroundColor: COLOR_NAME.GRAY_1,
+          borderTopLeftRadius: 20,
+          borderBottomLeftRadius: 20,
+          '& .ic-menu': {
+            display: 'none'
+          },
+          '& .ic-menu-active': {
+            display: 'block'
+          },
+          '& span': {
+            color: COLOR_NAME.DARK_GREEN
+          }
+        },
+        '& a': {
+          display: 'flex',
+          alignItems: 'center',
+          textDecoration: 'none'
+        },
+        '& span': {
+          marginTop: 5,
+          marginLeft: 15,
+          fontWeight: 600
+        },
+        '& img': {
+          width: 20,
+          '&.ic-menu': {},
+          '&.ic-menu-active': {
+            display: 'none'
+          }
+        }
       }
     }
   }
@@ -54,6 +98,21 @@ const SideBar = () => {
         <Grid item xs={12}>
           <div className={classes.menuContainer}>
             <h3 className="menu-title">Menu</h3>
+
+            <ul className="nav-menu">
+              <li className="active">
+                <Link to="">
+                  <img src="/static/images/icons/ic-home.svg" alt="home" className="ic-menu" />
+                  <img src="/static/images/icons/ic-home-active.svg" alt="home" className="ic-menu-active" />
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li>Profile</li>
+              <li>Rewards</li>
+              <li>Directory</li>
+              <li>Employee of the month</li>
+              <li>Leaders of Excellence</li>
+            </ul>
           </div>
         </Grid>
       </Grid>
